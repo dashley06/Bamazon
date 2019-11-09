969f20d69f0fb5205d8c3cbdfd9236da94f115b8
Ashley Clark <debashley06@gmail.com>
2019-11-09 18:27:13 -0500

    connection isnt working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,25 +3,63 @@ var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
     host: "localhost",
-    port: 2233,
+    port: "3306",
     user: "root",
-    password: "docker",
+    password: "",
     database: "bamazon_DB"
 });
 
 connection.connect(function(err){
     if (err) throw err;
-    bamazonQuestions();
+    console.log("Connected!")
+    start();
 });
+
+function start() {
+    connection.query("SELECT * FROM products", function(err, res) {
+      if (err) throw err;
+
+      for (var i = 0; i < res.length; i++) {
+        console.log(" - - - - - - - - - - - - - - - ")
+        console.log("item id: " + res[i].item_id)
+        console.log("item: " + res[i].product_name)
+        console.log("price: $" + res[i].price)
+    }
+        bamazonQuestions();
+})
+    
+      
+    };
 
 function bamazonQuestions(){
     inquirer
         .prompt([
+        {
             type: "input",
+            name: "askID",
+            message: "what is the ID of the item would you like to buy?",
+            validate: function(value) {
+                if (isNaN(value) === false) {
+                    return true;
+                } else {
+                    console.log('\nPlease enter a valid item ID.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: "number",
             name: "askAmount",
-            message: "what quantity of the item would you like to buy?"
-
-
+            message: "what quantity of the item would you like to buy?",
+            validate: function(value) {
+                if (isNaN(value) === false) {
+                    return true;
+                } else {
+                    console.log('\nPlease enter a valid quantity number');
+                    return false;
+                }
+            }
+        }
         ]).then(answers => {
 
 
